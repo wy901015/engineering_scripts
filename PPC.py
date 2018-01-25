db8379fd32dd3ecf5ea3cf6dff92d5854e6da7d6
Ocean <wy901015@gmail.com>
2018-01-25 14:04:32 +0800

    initial commit

---
+++ PPC.py
@@ -0,0 +1,85 @@
+import pandas as pd, datetime as dt
+import glob, os
+
+
+files = glob.glob("ppc_pile_cap v0.18d.xlsx")
+output_file = 'PPC Pile.xlsx'
+df_frame_assign = pd.DataFrame()
+df_Frame_force = pd.DataFrame()
+df_Frame_section = pd.DataFrame()
+df_frame_connet = pd.DataFrame()
+df_frame_coor = pd.DataFrame()
+
+for each in files:
+    #sheets = pd.ExcelFile(each).sheet_names
+	sheets = pd.ExcelFile(each).sheet_names
+
+	df_Frame_force = pd.read_excel(each, sheetname = 'Obj Geom - Areas 01 - General',skiprows=1)
+	df_frame_coor = pd.read_excel(each, sheetname = 'Obj Geom - Point Coordinates',skiprows=1)
+
+    
+df=df_Frame_force.ix[1:,['Area','Point1','Point2','Point3','Point4']]
+df2=pd.melt(df,id_vars=['Area'], value_vars=['Point1', 'Point2', 'Point3', 'Point4'])
+df2=df2.sort_values(['Area','value'], ascending=[False,True]).reset_index(drop=True)
+df2.rename(columns = {'value':'Point'}, inplace = True)
+df3 = df_frame_coor.loc[1:]
+df4 = pd.merge(df2, df3, how='left', on=['Point'])
+
+bp_xmin=df4.groupby(['Area']).GlobalX.min()
+bp_xmax=df4.groupby(['Area']).GlobalX.max()
+bp_ymin=df4.groupby(['Area']).GlobalY.min()
+bp_ymax=df4.groupby(['Area']).GlobalY.max()
+
+df_pilecap = pd.DataFrame()
+df_pilecap['X_min'] = bp_xmin
+df_pilecap['X_max'] = bp_xmax
+df_pilecap['Y_min'] = bp_ymin
+df_pilecap['Y_max'] = bp_ymax
+df_pilecap['Width'] = df_pilecap.X_max - df_pilecap.X_min
+df_pilecap['Depth'] = df_pilecap.Y_max - df_pilecap.Y_min
+df_pilecap['X_center'] = df_pilecap.X_min + df_pilecap.Width/2
+df_pilecap['Y_center'] = df_pilecap.Y_min + df_pilecap.Depth/2
+
+#df_pilecap = df_pilecap.reset_index()
+
+def is_point_inside_rect(pilecap, xx,yy):
+    bp = pd.DataFrame()
+    bp = pilecap.reset_index()
+    bp['x'] = xx.iloc[0]-bp.X_min
+    bp['y'] = bp.Y_max-yy.iloc[0]
+    bp['wd']=bp.Width*bp.Depth
+    bp['xy']=bp.x*bp.y
+    bp['ff']=0
+    bp.loc[(bp.xy >0) & (bp.xy < bp.wd) & (bp.y<bp.Depth) & (bp.x <bp.Width), 'ff'] = 1
+    aa=bp.Area[bp.ff==1]
+    return bp.Area[bp.ff==1].iloc[0]
+
+ii=0
+ppc = pd.DataFrame()
+ppc_name = []
+ppc=df3[df3.Point.str.contains('P')].reset_index(drop=True)
+for name in ppc.Point:
+    x = ppc.GlobalX[ppc.Point==name]
+    y = ppc.GlobalY[ppc.Point==name]
+    aa11=is_point_inside_rect(df_pilecap,x,y)
+    ppc_name.append(aa11)
+    ii+=1
+    print(ii)
+    
+ppc['Pile Cap'] = ppc_name
+
+writer = pd.ExcelWriter(output_file, engine='xlsxwriter')
+
+# Convert the dataframe to an XlsxWriter Excel object.
+
+ppc.index +=1
+
+ppc.to_excel(writer, sheet_name='PPC')
+
+df_pilecap_OUT = df_pilecap.reset_index()
+df_pilecap_OUT.index +=1
+
+df_pilecap_OUT.to_excel(writer, sheet_name='Pile Cap')
+
+writer.save()
+
