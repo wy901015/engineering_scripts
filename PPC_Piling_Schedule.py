77af835e520afd7fe53d97ae7f51b8a6e2efb29d
Ocean <wy901015@gmail.com>
2018-01-31 18:42:04 +0800

    add function to create column wall loading schedule

--- PPC_Piling_Schedule.py
+++ PPC_Piling_Schedule.py
@@ -1,20 +1,41 @@
 import pandas as pd 
-import datetime as dt
-import glob
 import os
+import glob
 
-df_piling_schedule = pd.DataFrame()
-df_point_reaction = pd.DataFrame()
-df_point_coordinate = pd.DataFrame()
+# User input
 files = glob.glob("MSC_P2 v1.0.xls")
 output_files = "PPC Piling Schedule"
+requiredCases = ["DL", "SDL", "LL", "WX", "WY", "WU", "WV", "EX", "EY"]
 
 for each in files:
-    df_point_reaction = pd.read_excel(each, sheetname="Nodal Reactions", skiprows=1)
-    df_point_coordinate = pd.read_excel(each, sheetname="Obj Geom - Point Coordinates", skiprows=1)
+    df_point_reaction = pd.read_excel(each, sheetname="Nodal Reactions", skiprows=1, converters={"OutputCase": str})
 
-# print(df_point_reaction.dtypes)
-df = df_point_reaction[df_point_reaction.Point.str.startswith("P", na=False)].reset_index(drop=True)
-df2 = df.loc[:, ["Point", "OutputCase", "Fz"]]
-df3 = df2.pivot(index = "Point", columns = "OutputCase", values = "Fz")
-print(df3)
+def returnNotMatches(a, b):
+    a = set(a)
+    b = set(b)
+    return list(b - a)
+
+def ppc_piling_schedule(): 
+  
+    df = df_point_reaction[df_point_reaction.Point.str.startswith("P", na=False)].reset_index(drop=True)
+    outputCases = df.OutputCase.unique()
+    unmatchesValue = returnNotMatches(outputCases, requiredCases)
+    point = df.Point.unique()
+    df_case = pd.DataFrame({"Point" : point})
+
+    for case in requiredCases:
+        if case in unmatchesValue:
+            df_na = pd.DataFrame(data={"Point" : point, case: 0}, columns=["Point", case])
+            df_case = df_case.merge(df_na, on="Point", how="right")
+            print("missing " + case)
+        else:
+            df2 = df[df["OutputCase"] == case].loc[:, ["Point", "Fz"]].rename(columns = {"Fz": case}).reset_index(drop=True)
+            df_case = df_case.merge(df2, on="Point", how="right")
+            print(case)
+
+    return df_case
+
+
+
+print(ppc_piling_schedule())
+
