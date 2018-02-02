2dd9dfb513331557d8c537b157b0807a6e7481eb
Ocean <wy901015@gmail.com>
2018-02-02 19:52:55 +0800

    try to separate the thing into functions

--- PPC_Piling_Schedule.py
+++ PPC_Piling_Schedule.py
@@ -32,51 +32,56 @@ def returnNotMatches(a, b):
     b = set(b)
     return list(b - a)
 
-df = df_point_reaction[df_point_reaction.Point.str.startswith("P", na=False)].reset_index(drop=True)
-df2 = df_point_coordinates[df_point_coordinates.Point.str.startswith("P", na=False) & ~df_point_coordinates.Point.str.contains("_", na=False)].reset_index(drop=True).loc[:, ["Point", "GlobalX", "GlobalY"]]
+   
+def returnPPCPilingSchedule():   
+    df = df_point_reaction[df_point_reaction.Point.str.startswith("P", na=False)].reset_index(drop=True)
+    df2 = df_point_coordinates[df_point_coordinates.Point.str.startswith("P", na=False) & ~df_point_coordinates.Point.str.contains("_", na=False)].reset_index(drop=True).loc[:, ["Point", "GlobalX", "GlobalY"]]
 
-outputCases = df.OutputCase.unique()
-unmatchesValue = returnNotMatches(outputCases, requiredCases)
-point = df.Point.unique()
-df_case = pd.DataFrame({"Point" : point})
+    outputCases = df.OutputCase.unique()
+    unmatchesValue = returnNotMatches(outputCases, requiredCases)
+    point = df.Point.unique()
+    df_case = pd.DataFrame({"Point" : point})
 
-for case in requiredCases:
-    if case in unmatchesValue:
-        df_na = pd.DataFrame(data={"Point" : point, case: 0}, columns=["Point", case])
-        df_case = df_case.merge(df_na, on="Point", how="right")
-        print("missing " + case)
-    else:
-        df_new = df[df["OutputCase"] == case].loc[:, ["Point", "Fz"]].rename(columns = {"Fz": case}).reset_index(drop=True)
-        df_case = df_case.merge(df_new, on="Point", how="right")
-        print(case)
+    for case in requiredCases:
+        if case in unmatchesValue:
+            df_na = pd.DataFrame(data={"Point" : point, case: 0}, columns=["Point", case])
+            df_case = df_case.merge(df_na, on="Point", how="right")
+            print("missing " + case)
+        else:
+            df_new = df[df["OutputCase"] == case].loc[:, ["Point", "Fz"]].rename(columns = {"Fz": case}).reset_index(drop=True)
+            df_case = df_case.merge(df_new, on="Point", how="right")
+            print(case)
 
-df_case['DDL'] = df_case["DL"] + df_case['SDL']
-df_schedule = df_case.merge(df2, on="Point", how="right")
+    df_case['DDL'] = df_case["DL"] + df_case['SDL']
+    df_schedule = df_case.merge(df2, on="Point", how="right")
 
-df_schedule["NSF"] = np.where(df_schedule.GlobalY < 51.355, nsf[51.355], np.where(df_schedule.GlobalY > 147.1, nsf[147.1], nsf["else"]))
-df_schedule["NSF"] = df_schedule["NSF"].astype(float, errors="raise")
-df_schedule["PileCP"] = pileCapacity * groupFactor
-df_schedule["PileCPW"] = df_schedule.PileCP * 1.25
+    df_schedule["NSF"] = np.where(df_schedule.GlobalY < 51.355, nsf[51.355], np.where(df_schedule.GlobalY > 147.1, nsf[147.1], nsf["else"]))
+    df_schedule["NSF"] = df_schedule["NSF"].astype(float, errors="raise")
+    df_schedule["PileCP"] = pileCapacity * groupFactor
+    df_schedule["PileCPW"] = df_schedule.PileCP * 1.25
 
-df_wind_max = df_schedule[["WX", "WY", "WU", "WV"]].max(axis = 1)
-df_seismic_max = df_schedule[["EX", "EY"]].max(axis = 1)
+    df_wind_max = df_schedule[["WX", "WY", "WU", "WV"]].max(axis = 1)
+    df_seismic_max = df_schedule[["EX", "EY"]].max(axis = 1)
 
-for slsCase, value in slsWithoutWind.items():
-    print ("Working load cases include: " + slsCase)
-    df_schedule[slsCase] = df_schedule.DDL * slsWithoutWind[slsCase]["D"] + df_schedule.LL * slsWithoutWind[slsCase]["L"] + df_wind_max * slsWithoutWind[slsCase]["W"] + df_seismic_max * slsWithoutWind[slsCase]["E"] + df_schedule.NSF * slsWithoutWind[slsCase]["NSF"] + df_schedule.UPLIFT * slsWithoutWind[slsCase]["U"] 
-    df_schedule["Ratio " + slsCase] = np.where(df_schedule["PileCP"] > df_schedule[slsCase], df_schedule[slsCase]/df_schedule["PileCP"], -1).astype(float)
-    df_schedule[slsCase] = df_schedule[slsCase].astype(float, errors="raise")
+    for slsCase, value in slsWithoutWind.items():
+        print ("Working load cases include: " + slsCase)
+        df_schedule[slsCase] = df_schedule.DDL * slsWithoutWind[slsCase]["D"] + df_schedule.LL * slsWithoutWind[slsCase]["L"] + df_wind_max * slsWithoutWind[slsCase]["W"] + df_seismic_max * slsWithoutWind[slsCase]["E"] + df_schedule.NSF * slsWithoutWind[slsCase]["NSF"] + df_schedule.UPLIFT * slsWithoutWind[slsCase]["U"] 
+        df_schedule["Ratio " + slsCase] = np.where(df_schedule["PileCP"] > df_schedule[slsCase], df_schedule[slsCase]/df_schedule["PileCP"], -1).astype(float)
+        df_schedule[slsCase] = df_schedule[slsCase].astype(float, errors="raise")
 
-for slsCase, value in slsWithWind.items():
-    print ("Working load cases include: " + slsCase)
-    df_schedule[slsCase] = df_schedule.DDL * slsWithWind[slsCase]["D"] + df_schedule.LL * slsWithWind[slsCase]["L"] + df_wind_max * slsWithWind[slsCase]["W"] + df_seismic_max * slsWithWind[slsCase]["E"] + df_schedule.NSF * slsWithWind[slsCase]["NSF"] + df_schedule.UPLIFT * slsWithWind[slsCase]["U"] 
-    df_schedule["Ratio " + slsCase] = np.where(df_schedule["PileCPW"] > df_schedule[slsCase], df_schedule[slsCase]/df_schedule["PileCPW"], -1).astype(float)
-    df_schedule[slsCase] = df_schedule[slsCase].astype(float, errors="raise")
+    for slsCase, value in slsWithWind.items():
+        print ("Working load cases include: " + slsCase)
+        df_schedule[slsCase] = df_schedule.DDL * slsWithWind[slsCase]["D"] + df_schedule.LL * slsWithWind[slsCase]["L"] + df_wind_max * slsWithWind[slsCase]["W"] + df_seismic_max * slsWithWind[slsCase]["E"] + df_schedule.NSF * slsWithWind[slsCase]["NSF"] + df_schedule.UPLIFT * slsWithWind[slsCase]["U"] 
+        df_schedule["Ratio " + slsCase] = np.where(df_schedule["PileCPW"] > df_schedule[slsCase], df_schedule[slsCase]/df_schedule["PileCPW"], -1).astype(float)
+        df_schedule[slsCase] = df_schedule[slsCase].astype(float, errors="raise")
 
-df_ratio = df_schedule.filter(regex="Ratio")
-df_schedule["Ratio"] = df_ratio.max(axis=1)
-df_schedule_out = df_schedule.loc[:, ["Point", "NSF", "DDL", "LL", "WX", "WY", "WU", "WV", "EX", "EY", "UPLIFT"] + list(slsWithWind.keys()) + ["PileCP", "PileCPW", "Ratio"]]
-df_schedule_out.to_excel(output_files, sheet_name="PPC Piling Schedule", index=False)
+    df_ratio = df_schedule.filter(regex="Ratio")
+    df_schedule["Ratio"] = df_ratio.max(axis=1)
+    df_schedule_out = df_schedule.loc[:, ["Point", "NSF", "DDL", "LL", "WX", "WY", "WU", "WV", "EX", "EY", "UPLIFT"] + list(slsWithWind.keys()) + ["PileCP", "PileCPW", "Ratio"]]
+    
+    return df_schedule_out
+    
+    df_schedule_out.to_excel(output_files, sheet_name="PPC Piling Schedule", index=False)
 
 # Print out to dxf
 df_to_dxf = df_schedule.loc[df_schedule["Ratio"] < targetUtilization , ["Point", "GlobalX", "GlobalY"]]
